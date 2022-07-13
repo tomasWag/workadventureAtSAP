94ee207a15f2a935498d762d8f29f949d731ca6b
tomasWag <tomas.wagner1@web.de>
2022-07-13 14:14:22 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
 
 console.log('Script started successfully');
 
